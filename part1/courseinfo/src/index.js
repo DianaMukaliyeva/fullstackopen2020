eabe423951a3feec83352c2820b05813dd2f400a
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-20 00:00:00 +0300

    build: :construction: course information, step 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,25 +5,22 @@ const Header = (props) => {
     return <h1>{props.course}</h1>
 }
 
-const Content = (props) => {
+const Part = (props) => {
+    return <p>{props.part} {props.exercises}</p>
+}
 
-    return <>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
-    </>
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
+        </div>
+    )
 }
 
 const Total = (props) => {
-    return <>
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
+    return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 }
 
 const App = () => {
