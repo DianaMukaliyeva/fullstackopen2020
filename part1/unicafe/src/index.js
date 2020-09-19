2ed70ae5d0b2e2f1dfcd8ea7109ccb766d831dad
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-20 02:11:53 +0300

    build: :construction: 1.7 unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,6 +13,24 @@ const Result = ({ title, result }) => (
     <div>{title} {result}</div>
 )
 
+const Statistic = ({ title, good, neutral, bad }) => {
+    const all = good + neutral + bad;
+    const average = all !== 0 ? (good - bad) / all : 0;
+    const positive = all !== 0 ? good / all * 100 + ' %' : 0 + ' %';
+
+    return (
+        <div>
+            <Header content={title} />
+            <Result title="good" result={good} />
+            <Result title="neutral" result={neutral} />
+            <Result title="bad" result={bad} />
+            <Result title="all" result={all} />
+            <Result title="average" result={average} />
+            <Result title="positive" result={positive} />
+        </div>
+    )
+}
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0);
@@ -25,10 +43,7 @@ const App = () => {
             <Button handleClick={() => { setGood(good + 1) }} name="good" />
             <Button handleClick={() => { setNeutral(neutral + 1) }} name="neutral" />
             <Button handleClick={() => { setBad(bad + 1) }} name="bad" />
-            <Header content='statistic' />
-            <Result title="good" result={good} />
-            <Result title="neutral" result={neutral} />
-            <Result title="bad" result={bad} />
+            <Statistic title="statistics" good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
