121e0d4b9d1ecb5ab634018a397cb0ae9b3a66ed
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-23 01:22:46 +0300

    build: :construction: 2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Contact from './components/Contact';
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+    const [newName, setNewName] = useState('');
+
+    const handleChange = event => {
+        setNewName(event.target.value);
+    };
+
+    const addContact = event => {
+        event.preventDefault();
+        const newContact = { name: newName };
+        setPersons(persons.concat(newContact));
+        setNewName('');
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addContact}>
+                <div>
+                    name: <input value={newName} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type='submit'>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person => (
+                <Contact key={person.name} person={person} />
+            ))}
+        </div>
+    );
+};
+
+export default App;
---
+++ part2/phonebook/src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = ({ person }) => {
+    return <div>{person.name}</div>;
+};
+
+export default Contact;
