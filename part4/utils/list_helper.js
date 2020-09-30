3414c86f07ed09007eee157e051b078fb2914215
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-09-30 17:11:04 +0300

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = blogs => {
     return 1;
 };
 
+const totalLikes = blogs => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return Object.keys(blogs).length === 0 ? 0 : blogs.map(blog => blog.likes).reduce(reducer);
+};
+
 module.exports = {
     dummy,
+    totalLikes,
 };
