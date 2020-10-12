66c4df47047363b3a2cba75b5de5ef67789d6916
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-12 18:09:38 +0300

    4.17: bloglist expansion, step5

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -26,7 +26,7 @@ usersRoute.post('/', async (request, response) => {
 });
 
 usersRoute.get('/', async (request, response) => {
-    const users = await User.find({});
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 });
 
     response.json(users);
 });
