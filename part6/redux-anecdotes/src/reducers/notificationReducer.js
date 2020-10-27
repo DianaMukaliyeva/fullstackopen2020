abc6ca35abdef0c4a30aa522f9be08e30576185e
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-10-27 19:22:24 +0200

    6.11 Better anecdotes, step9

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,12 +1,29 @@
-const initialState = 'message';
+const initialState = '';
 
 const notificationReducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
   switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.data;
+    case 'CLEAR_NOTIFICATION':
+      return initialState;
     default:
       return state;
   }
+};
+
+export const setNotification = (message) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    data: message,
+  };
+};
+
+export const clearNotification = () => {
+  return {
+    type: 'CLEAR_NOTIFICATION',
+  };
 };
 
 export default notificationReducer;
