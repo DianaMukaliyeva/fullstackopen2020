2b97a19845c1fbe6548eba96ce1517bb0e4e785e
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-11-03 02:52:41 +0200

    6.21 anecdotes, the grand finale

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,26 +1,25 @@
-const initialState = '';
+const initialState = { message: '', id: 0 };
 
 const notificationReducer = (state = initialState, action) => {
-  console.log('state now: ', state);
-  console.log('action', action);
   switch (action.type) {
     case 'SET_NOTIFICATION':
-      return action.data;
+      return { id: action.data.id, message: action.data.message };
     case 'CLEAR_NOTIFICATION':
-      return initialState;
+      return { ...state, message: '' };
     default:
       return state;
   }
 };
 
-export const setNotification = (message = '', seconds = 5) => async (dispatch) => {
-  dispatch({
-    type: 'SET_NOTIFICATION',
-    data: message,
-  });
-  setTimeout(() => {
+export const setNotification = (message = '', seconds = 5) => async (dispatch, getState) => {
+  clearTimeout(getState().notification.id);
+  let id = setTimeout(() => {
     dispatch(clearNotification());
   }, seconds * 1000);
+  dispatch({
+    type: 'SET_NOTIFICATION',
+    data: { message: message, id: id },
+  });
 };
 
 export const clearNotification = () => {
