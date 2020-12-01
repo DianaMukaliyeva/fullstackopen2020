45ce2262cfa31f9e7ae84250d695850e40dafb4b
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-12-02 01:04:12 +0200

    8.8: Authors view

---
+++ part8/frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { ALL_AUTHORS } from './queries';
+
+const App = () => {
+  const [page, setPage] = useState('authors');
+  const result = useQuery(ALL_AUTHORS);
+
+  // useEffect(()=>{
+
+  // }, page)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} result={result} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  );
+};
+
+export default App;
