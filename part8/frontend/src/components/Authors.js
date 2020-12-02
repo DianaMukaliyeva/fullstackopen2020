d3e34ddecd4d76d9f71eb71133831a5aa2401751
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-12-02 04:36:08 +0200

    8.11: Authors birth year

--- part8/frontend/src/components/Authors.js
+++ part8/frontend/src/components/Authors.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries';
 
 const Authors = ({ show, result }) => {
+  const [year, setYear] = useState('');
+  const [name, setName] = useState('');
+  const [changeBirthYear] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   if (!show) {
     return null;
   }
+
   if (result.loading) {
     return <div>loading...</div>;
   }
+
   const authors = result.data.allAuthors;
+
+  const updateBirthYear = (event) => {
+    event.preventDefault();
+    changeBirthYear({ variables: { name, born: parseInt(year) } });
+    setYear('');
+    setName('');
+  };
 
   return (
     <div>
@@ -28,6 +48,21 @@ const Authors = ({ show, result }) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birth year</h3>
+      <form onSubmit={updateBirthYear}>
+        <div>
+          <label>name:</label>
+          <input value={name} onChange={({ target }) => setName(target.value)}></input>
+        </div>
+        <div>
+          <label>born:</label>
+          <input
+            type="number"
+            value={year}
+            onChange={({ target }) => setYear(target.value)}></input>
+        </div>
+        <button>update author</button>
+      </form>
     </div>
   );
 };
