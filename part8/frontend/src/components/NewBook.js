832ec667692430e1e477810a1cf83ac18694c4ad
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-12-05 15:37:25 +0200

    8.22 Up to date cache and book recommendations

--- part8/frontend/src/components/NewBook.js
+++ part8/frontend/src/components/NewBook.js
@@ -8,6 +8,7 @@ const NewBook = ({ show, notify }) => {
   const [published, setPublished] = useState('');
   const [genre, setGenre] = useState('');
   const [genres, setGenres] = useState([]);
+  // when new book is added, all books and all authors are updated
   const [addBook] = useMutation(ADD_BOOK, {
     refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
   });
